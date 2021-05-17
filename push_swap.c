0dc12d98dcb94960db3fea288df0555ccdee42e4
Neben <neben.everything@gmail.com>
2021-05-17 02:33:38 +0200

    .

--- push_swap.c
+++ push_swap.c
@@ -16,12 +16,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void	push_swap(int *a, int *b)
+void	push_swap(long *a, int *b)
 {
-	while (*a)
+	int i = 0;
+
+	while (a[i])
 	{
-		printf("%d ", *a);
-		a++;
+		printf("%d ", a[i]);
+		i++;
 	}
 }
 
@@ -30,8 +32,8 @@ int	init_list(char **list, long *stack_a, int size)
 	stack_a[size] = '\0';
 	while (size--)
 	{
-		if (!ft_is_number(list[size + 1]))
-			return (0);
+	//	if (!ft_is_number(list[size + 1]))
+	//		return (0);
 		stack_a[size] = atol(list[size + 1]);
 		if (stack_a[size] < INT_MIN || stack_a[size] > INT_MAX)
 			return (0);
@@ -43,13 +45,17 @@ int	main(int argc, char **argv)
 {
 	long	*stack_a;
 
-	stack_a = malloc(sizeof(int) * argc);
-	if (stack_a)
+	if (argc >= 2)
 	{
-		if (init_list(argv, stack_a, argc - 1))
+		stack_a = malloc(sizeof(int) * argc);
+		if (stack_a)
 		{
-			push_swap((int *)stack_a, NULL);
-			return (1);
+			if (init_list(argv, stack_a, argc - 1))
+			{
+				printf("init!\n#####################\n");
+				push_swap(stack_a, NULL);
+				return (1);
+			}
 		}
 	}
 	printf("Error\n");
